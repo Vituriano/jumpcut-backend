de13e38ab911f1833e31bc560f767fd2e4cf264d
Vituriano <viturianooliveira@gmail.com>
2024-03-12 10:25:15 -0300

    fix cors error

--- api/main.py
+++ api/main.py
@@ -3,12 +3,25 @@ import uvicorn
 from models.audio import Audio
 from models.video import Video
 from routes.video_routes import video_router
+from fastapi.middleware.cors import CORSMiddleware
 
 VIDEOS_DICT = {}
 
 app = FastAPI(version='0.1.0')
 video_model = Video()
 audio_model = Audio()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     router=video_router(video_model, audio_model),
